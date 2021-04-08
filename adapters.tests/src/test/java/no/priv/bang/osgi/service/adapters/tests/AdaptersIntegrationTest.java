5800fff9b0a830c1069dcbd785114cb93e880488
Steinar Bang <sb@dod.no>
2021-04-08 22:54:36 +0200

    Format feature repo maven coordinates vertically in adapters integration test, to make them easier to read

--- adapters.tests/src/test/java/no/priv/bang/osgi/service/adapters/tests/AdaptersIntegrationTest.java
+++ adapters.tests/src/test/java/no/priv/bang/osgi/service/adapters/tests/AdaptersIntegrationTest.java
@@ -36,7 +36,12 @@ public class AdaptersIntegrationTest extends KarafTestSupport {
 
     @Configuration
     public Option[] config() {
-        final MavenArtifactUrlReference adaptersFeatureRepo = maven().groupId("no.priv.bang.osgi.service.adapters").artifactId("karaf").version("LATEST").type("xml").classifier("features");
+        final MavenArtifactUrlReference adaptersFeatureRepo = maven()
+            .groupId("no.priv.bang.osgi.service.adapters")
+            .artifactId("karaf")
+            .version("LATEST")
+            .type("xml")
+            .classifier("features");
         Option[] options = new Option[] {
             features(adaptersFeatureRepo)
         };
