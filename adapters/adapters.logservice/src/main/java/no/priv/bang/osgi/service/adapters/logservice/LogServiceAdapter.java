9067657fb50b04d6989c2ddf10d98d2e2fcba656
Steinar Bang <sb@dod.no>
2024-06-13 21:51:39 +0200

    Use var for local variable

--- adapters/adapters.logservice/src/main/java/no/priv/bang/osgi/service/adapters/logservice/LogServiceAdapter.java
+++ adapters/adapters.logservice/src/main/java/no/priv/bang/osgi/service/adapters/logservice/LogServiceAdapter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2021 Steinar Bang
+ * Copyright 2017-2024 Steinar Bang
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -125,7 +125,7 @@ public class LogServiceAdapter implements LogService {
 
     private void sendSavedLogMessagesToRealLogService(LogService log) {
         if(log != null) {
-            for (SavedLogMessage savedLogMessage : savedLogMessages) {
+            for (var savedLogMessage : savedLogMessages) {
                 savedLogMessage.sendToService(log);
             }
 
